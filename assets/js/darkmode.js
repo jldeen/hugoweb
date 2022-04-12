30763bb4de43cae58359ea87a13c63bb8acfdd44
jldeen <jessicadeen@me.com>
2022-04-12 13:26:51 -0700

    clean up ion classList and add toggle transition for theme
    Signed-off-by: jldeen <jessicadeen@me.com>

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,19 +1,25 @@
 // https://tomhazledine.com/dark-mode/
 
+// Set Dark Mode
 const setDarkMode = (active = false) => {
   const wrapper = document.querySelector(":root");
   const icon = document.getElementById("dark-mode-toggle");
   if (active) {
     wrapper.setAttribute("data-theme", "dark");
-    icon.className = "ion js__dark-mode-toggle dark-mode-toggle ion-md-moon";
+    // If icon class list has "ion-md-sunny", remove it so "ion-md-moon" can be added
+    if (icon.classList.contains("ion-md-sunny")) {
+      icon.classList.remove("ion-md-sunny");
+    }
+    icon.classList.add("ion-md-moon");
     localStorage.setItem("theme", "dark");
   } else {
     wrapper.setAttribute("data-theme", "light");
-    icon.className = "ion js__dark-mode-toggle dark-mode-toggle ion-md-sunny";
+    icon.classList.add("ion-md-sunny");
     localStorage.setItem("theme", "light");
   }
 };
 
+// Query user preference and see if theme preference has been set
 const query = window.matchMedia("(prefers-color-scheme: dark)");
 const themePreference = localStorage.getItem("theme");
 
@@ -27,13 +33,18 @@ if (themePreference === "light") {
   active = false;
 }
 
+// Set theme based on user preference or override
 setDarkMode(active);
+
+// Watch for user preference changes
 query.addEventListener("change", (e) => setDarkMode(e.matches));
 
+// Toggle Dark Mode Logic
 const toggleDarkMode = () => {
   const theme = document.querySelector(":root").getAttribute("data-theme");
   setDarkMode(theme === "light");
 };
 
+// Toggle Dark Mode Button with event listener
 const toggleButton = document.querySelector(".js__dark-mode-toggle");
 toggleButton.addEventListener("click", toggleDarkMode);
