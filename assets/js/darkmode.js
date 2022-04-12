fc5080412dafa229ce542c9ddb6f81225cb9a8b5
jldeen <jessicadeen@me.com>
2022-04-12 11:05:09 -0700

    Check in progress

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,9 +1,34 @@
-if (window.matchMedia('(prefers-color-scheme)').media !== 'not all') {
-  console.log('🎉 Dark mode is supported');
+const setDarkMode = (active = false) => {
+  const wrapper = document.querySelector(":root");
+  if (active) {
+    wrapper.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    wrapper.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+  }
+};
+
+const query = window.matchMedia("(prefers-color-scheme: dark)");
+const themePreference = localStorage.getItem("theme");
+
+let active = query.matches;
+
+if (themePreference === "dark") {
+  active = true;
 }
 
-const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-darkModeMediaQuery.addEventListener("change", (e) => {
-  const darkModeOn = e.matches;
-  console.log(`Dark mode is ${darkModeOn ? '🌒 on' : '☀️ off'}.`);
-});
+if (themePreference === "light") {
+  active = false;
+}
+
+setDarkMode(active);
+query.addEventListener("change", (e) => setDarkMode(e.matches));
+
+const toggleDarkMode = () => {
+  const theme = document.querySelector(":root").getAttribute("data-theme");
+  setDarkMode(theme === "light");
+};
+
+const toggleButton = document.querySelector(".js__dark-mode-toggle");
+toggleButton.addEventListener("click", toggleDarkMode);
